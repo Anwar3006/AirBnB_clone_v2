20cba93ba0a825c74160d4017b3f5ef208ed5a69
Anwar3006 <anwarsadat.d2@gmail.com>
2023-02-05 15:34:36 +0000

    Web Framework

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,16 +11,21 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def heelo():
+    """display “Hello HBNB!”"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """display “HBNB”"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def C(text):
+    """ display “C ”, followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
     if text:
         new = ' '.join(text.split('_'))
         return "C %s" % new
@@ -26,6 +36,9 @@ def C(text):
 @app.route("/python/", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
+    """display “Python ”, followed by the value of the text variable
+    (replace underscore _ symbols with a space ) 
+    """
     if text:
         new = ''.join(text.split('_'))
         return "Python %s" % new
