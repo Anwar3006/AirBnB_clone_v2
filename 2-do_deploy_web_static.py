18b480634dbf45d5a8e893635f4c671ab490933a
Anwar3006 <anwarsadat.d2@gmail.com>
2023-02-05 13:51:54 +0000

    Airbnb_clone_v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 # Fabfile to distribute an archive to a web server.
-from datetime import datetime
-from fabric.api import *
 from os import path
-
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
 env.hosts = ['100.26.160.22', '100.25.202.194']
 
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
     if path.exists(archive_path):
         archive_file = archive_path.split('/')[1]
