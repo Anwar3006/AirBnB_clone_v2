98bc2693f8780df5b586ec83f28f7a86626e3ce4
Anwar3006 <anwarsadat.d2@gmail.com>
2023-02-05 13:53:27 +0000

    Airbnb_clone_v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,43 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
+from datetime import datetime
+from fabric.api import *
 from os import path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
 
-env.hosts = ['100.26.160.22', '100.25.202.194']
+
+env.hosts = ['35.229.93.37', '54.196.213.127']
+
+
+def do_pack():
+    """Generates a .tgz archive from the contents
+    of the web_static folder of this repository.
+    """
+
+    d = datetime.now()
+    now = d.strftime('%Y%m%d%H%M%S')
+
+    local("mkdir -p versions")
+    local("tar -czvf versions/web_static_{}.tgz web_static".format(now))
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
+    """Distributes an .tgz archive through web servers
     """
-    if path.exists(archive_path):
-        archive_file = archive_path.split('/')[1]
-        upload_path = f'/tmp/{archive_file}'
-        extract_folder = archive_file.split('.')[0]
-        extract_path = f'/data/web_static_releases/{extract_folder}/'
 
-        put(archive_path, upload_path)
-        run("mkdir -p {}".format(extract_path))
-        run("tar -xzf {} -C {}".format(upload_path, extract_path))
-        run("rm -rf {}".format(upload_path))
-        run("mv -f {}web_static/* {}".format(extract_path, extract_path))
-        run("rm -rf {}web_static".format(extract_path))
-        run("rm -f /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(extract_path))
+    if path.exists(archive_path):
+        archive = archive_path.split('/')[1]
+        a_path = "/tmp/{}".format(archive)
+        folder = archive.split('.')[0]
+        f_path = "/data/web_static/releases/{}/".format(folder)
+
+        put(archive_path, a_path)
+        run("mkdir -p {}".format(f_path))
+        run("tar -xzf {} -C {}".format(a_path, f_path))
+        run("rm {}".format(a_path))
+        run("mv -f {}web_static/* {}".format(f_path, f_path))
+        run("rm -rf {}web_static".format(f_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(f_path))
 
         return True
+
     return False
