0c8e86db7485906e7b6cca9d0468d554f92b0319
Anwar3006 <anwarsadat.d2@gmail.com>
2023-02-06 17:18:34 +0000

    Web Framework

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,42 +6,42 @@ Routes:
 """
 from flask import Flask
 
-app = Flask(__name__)
-
-
-@app.route("/", strict_slashes=False)
-def heelo():
-    """display “Hello HBNB!”"""
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """display “HBNB”"""
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def C(text):
-    """ display “C ”, followed by the value of the text variable
-    (replace underscore _ symbols with a space )
-    """
-    if text:
-        new = ' '.join(text.split('_'))
-        return "C %s" % new
-    else:
-        return 404
-
-
-@app.route("/python/", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
-    if text:
-        new = ''.join(text.split('_'))
-        return "Python %s" % new
-    else:
-        return "Python %s" % text
-
-
 if __name__ == "__main__":
+    app = Flask(__name__)
+
+
+    @app.route("/", strict_slashes=False)
+    def heelo():
+        """display “Hello HBNB!”"""
+        return "Hello HBNB!"
+
+
+    @app.route("/hbnb", strict_slashes=False)
+    def hbnb():
+        """display “HBNB”"""
+        return "HBNB"
+
+
+    @app.route("/c/<text>", strict_slashes=False)
+    def C(text):
+        """ display “C ”, followed by the value of the text variable
+        (replace underscore _ symbols with a space )
+        """
+        if text:
+            new = ' '.join(text.split('_'))
+            return "C %s" % new
+        else:
+            return 404
+
+
+    @app.route("/python/", strict_slashes=False)
+    @app.route("/python/<text>", strict_slashes=False)
+    def python(text="is cool"):
+        if text:
+            new = ''.join(text.split('_'))
+            return "Python %s" % new
+        else:
+            return "Python %s" % text
+
+
     app.run(debug=True, host="0.0.0.0")
