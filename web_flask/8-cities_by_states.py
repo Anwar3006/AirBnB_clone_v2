56b614fca53511313a12b29f438dfb8f50be2769
Anwar3006 <anwarsadat.d2@gmail.com>
2023-02-06 16:56:55 +0000

    Web Framework

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'
-"""
+from models import storage
 from flask import Flask, render_template
+from models.city import City
+from models.state import State
 
 app = Flask(__name__)
 
@@ -59,7 +57,47 @@ def number_template(n):
     """display a HTML page only if n is an integer"""
     if type(n) is int:
         return render_template("5-number.html", n=n)
-    return 404
+    return 404    
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def numberEvenOdd(n):
+    """display a HTML page only if n is an integer"""
+    if type(n) is int:    
+        return render_template("6-number_odd_or_even.html", n=n)
+    else:
+        return 404
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a HTML page of the States
+    """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page of the States and the
+    Cities by State
+    """
+    states = storage.all(State).values()
+    cities = list()
+
+    for state in states:
+        for city in state.cities:
+            cities.append(city)
+
+    return render_template('8-cities_by_states.html',
+                           states=states, state_cities=cities)    
+
+
+@app.teardown_appcontext
+def teardown_db(error):
+    """Closes the database again at the end of the request.
+    """
+    storage.close()
 
 
 if __name__ == "__main__":
