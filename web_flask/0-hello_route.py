9bd4820c04e114814108ecc2b2969b68b9b52cc6
Anwar3006 <anwarsadat.d2@gmail.com>
2023-02-06 17:01:30 +0000

    Web Framework

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,14 +6,14 @@ Routes:
 """
 from flask import Flask
 
-app = Flask(__name__)
-
-
-@app.route("/", strict_slashes=False)
-def heelo():
-    """display “Hello HBNB!”"""
-    return "Hello HBNB!"
-
-
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app = Flask(__name__)
+
+
+    @app.route("/", strict_slashes=False)
+    def heelo():
+        """display “Hello HBNB!”"""
+        return "Hello HBNB!"
+
+
+app.run(debug=True, host="0.0.0.0")
