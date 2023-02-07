0532c629475f137d978f637f225c5e59d0ac867c
Anwar3006 <anwarsadat.d2@gmail.com>
2023-02-07 14:01:12 +0000

    Web Framework

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,14 +1,16 @@
 -- Set ALL privileges on the database `hbnb_dev_db` to the `hbnb_dev` user.
 -- Set SELECT privileges on the database `performance_schema` to the `hbnb_dev` user.
 
-CREATE DATABASE IF NOT EXISTS 'hbnb_dev_db';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER
-   IF NOT EXISTS 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+    IF NOT EXISTS 'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES
-   ON hbnb_dev_db.*
-   TO 'hbnb_dev'@'localhost';
+   ON `hbnb_dev_db`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT
-   ON performance_schema.*
-   TO 'hbnb_dev'@'localhost';
+   ON `performance_schema`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
